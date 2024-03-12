15b570939e0a2e4fc43205f6b412eaad1aa41c11
Lalit-Mahajan <139694592+Lalit-Mahajan@users.noreply.github.com>
2024-03-12 17:19:51 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,8 @@ const Home = () => {
     <Layout>
         <div className="home" style={{backgroundImage: `url(${Banner})`}}>
           <div className="headerContainer">
-           <h1>Food Website</h1>
-           <p>Best Food In India</p>
+           <h1>My Food Website</h1>
+           <p>Best Food In Indian</p>
            <Link to="/menu">
             <button>
               ORDER NOW 💁
